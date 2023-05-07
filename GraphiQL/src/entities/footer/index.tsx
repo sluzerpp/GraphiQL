fa105059e61b88622054e1d52c18c10f1c2e4b9d
Margarita <Margarita>
2023-05-07 23:42:24 +0300

    feat: add localization

--- GraphiQL/src/entities/footer/index.tsx
+++ GraphiQL/src/entities/footer/index.tsx
@@ -1,13 +1,19 @@
 import { AiFillGithub } from 'react-icons/ai';
 import school from '../../assets/svg/logo-rs.svg';
 import classes from './style.module.scss';
-import { creatorsData } from 'data';
+import { CreatorType } from 'type';
+import { useTranslation } from 'react-i18next';
 
 export default function Footer({}) {
+  const { t } = useTranslation();
   return (
     <footer className={`${classes.footer} container`}>
       <div className={classes.footer__git}>
-        {creatorsData.map((item) => (
+        {(
+          t('welcome.creators', { returnObjects: true }) as {
+            content: CreatorType[];
+          }
+        ).content.map((item) => (
           <a key={item.git} href={`https://github.com/${item.git}`}>
             <AiFillGithub className={classes.footer__gititem} />
           </a>
--- GraphiQL/src/main.tsx
+++ GraphiQL/src/main.tsx
@@ -1,4 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from 'app';
+import React from 'react';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+import './i18n';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <React.Suspense fallback="Loading...">
+      <App />
+    </React.Suspense>
+  </React.StrictMode>
+);
