a10bd9d5d67f5e48daf5b2d8dc47f7bda05c0e87
imikh1991 <ilya.mikhaylyukov@gmail.com>
2023-05-13 22:26:48 +0700

    feat:react-hook-form

--- GraphiQL/src/entities/Register.tsx
+++ GraphiQL/src/entities/Register.tsx
@@ -1,98 +1,108 @@
-import { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useState } from 'react';
+// import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
-import { auth, registerWithEmailAndPassword, signInWithGoogle } from './firebase';
+import { registerWithEmailAndPassword, signInWithGoogle } from './firebase';
+import { useForm } from 'react-hook-form';
 import './Register.css';
-// import { UserValues } from '../entities/types';
+
+type RegisterFormData = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 function Register() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    // reset,
+  } = useForm<RegisterFormData>();
+  const [error, setErrorFirebase] = useState('');
   const [loading, setLoading] = useState(false);
-  const [user] = useAuthState(auth);
+  // const [user] = useAuthState(auth);
 
   const navigate = useNavigate();
 
-  useEffect(() => {
+  /*useEffect(() => {
     if (user) navigate('/dashboard');
   }, [user, navigate]);
+  */
+  const onSubmit = async (data: RegisterFormData) => {
+    // Validate Fuctions
+    const validateEmail = (email: string) => {
+      // A regular expression for checking if the email is valid
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    };
 
-  // validation
-  const validateEmail = (email: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-
-  const validatePassword = (password: string) => {
-    const passwordRegex = /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/;
-    return passwordRegex.test(password);
-  };
-
-  const register = () => {
-    // Validate the email and password fields
-    if (!validateEmail(email)) {
-      setError('Please enter a valid email address');
-      setLoading(false);
+    const validatePassword = (password: string) => {
+      // A regular expression for checking if the password is strong enough
+      const passwordRegex = /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/;
+      return passwordRegex.test(password);
+    };
+    if (!validateEmail(data.email)) {
+      setError('email', { message: 'Please enter a valid email address' });
       return;
     }
 
-    if (!validatePassword(password)) {
-      setError(
-        'Please enter a strong password with at least 8 characters, one letter, one digit, and one special character'
-      );
-      setLoading(false);
+    if (!validatePassword(data.password)) {
+      setError('password', {
+        message:
+          'Please enter a strong \npassword with at least 8 characters,\n one letter, one digit, and one special \ncharacter',
+      });
       return;
     }
-    if (password && email) {
-      // Register
-      try {
-        setError('');
-        setLoading(true);
-        registerWithEmailAndPassword(name, email, password);
-        navigate('/dashboard');
-      } catch {
-        setError('Failed to Register');
-      }
-      setLoading(false);
-      alert('RegisterWithEmailAndPassword-> Success!');
-    } else {
-      alert('Failed Try Again');
+
+    try {
+      setErrorFirebase('');
+      setLoading(true);
+      await registerWithEmailAndPassword(data.name, data.email, data.password);
+      navigate('/');
+    } catch {
+      setErrorFirebase('Failed to Register');
     }
+
+    setLoading(false);
+    alert('RegisterWithEmailAndPassword-> Success!');
   };
 
   return (
     <div className="register">
       <div className="register__container">
-        <input
-          type="text"
-          className="register__textBox"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Full Name"
-        />
-        <input
-          type="text"
-          className="register__textBox"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="E-mail Address"
-        />
-        <input
-          type="password"
-          className="register__textBox"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <div className="register__error">{error && <div>{error}</div>}</div>
-        <button disabled={loading} className="register__btn" onClick={register}>
-          Register
-        </button>
-        <button className="register__btn register__google" onClick={signInWithGoogle}>
-          Register with Google
-        </button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="text"
+            className="register__textBox"
+            {...register('name')}
+            placeholder="Full Name"
+          />
+          <div className="register__error">{errors.name && <div>{errors.name.message}</div>}</div>
+          <input
+            type="text"
+            className="register__textBox"
+            {...register('email', { required: true })}
+            placeholder="E-mail Address"
+          />
+          <div className="register__error">{errors.email && <div>{errors.email.message}</div>}</div>
+          <input
+            type="password"
+            className="register__textBox"
+            {...register('password', { required: true })}
+            placeholder="Password"
+          />
+          <div className="register__error">
+            {errors.password && <div>{errors.password.message}</div>}
+          </div>
+          <div className="register__error">{error && <div>{error}</div>}</div>
+          <button disabled={loading} className="register__btn" type="submit">
+            Register
+          </button>
+          <button className="register__btn register__google" onClick={signInWithGoogle}>
+            Register with Google
+          </button>
+        </form>
         <div>
           Already have an account? <Link to="/auth">Login</Link> now.
         </div>
