947b1620d224cc82f9c906dd6e06bd46b5f99676
sluzerpp <bistboy.es@gmail.com>
2023-05-19 16:47:15 +0300

    fix: docs nonullable args

--- GraphiQL/src/features/GraphQLDocs/GraphQLObject/helpers/getArgString.ts
+++ GraphiQL/src/features/GraphQLDocs/GraphQLObject/helpers/getArgString.ts
@@ -4,7 +4,7 @@ import getOutputTypeString from './getTypeString';
 const getArgString = (object: GraphQLArgument): string[] => {
   const type = object.type;
   if (type instanceof GraphQLNonNull) {
-    return [object.name, getOutputTypeString(type.ofType)];
+    return [object.name, `${getOutputTypeString(type.ofType)}!`];
   }
   return [object.name, getOutputTypeString(type)];
 };
